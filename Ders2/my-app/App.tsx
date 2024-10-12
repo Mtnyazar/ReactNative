b65d3a3cbe71ad69c9c46ae770c0286501852480
Mtnyazar <94050730+Mtnyazar@users.noreply.github.com>
2024-10-12 23:42:40 +0300

    revize2

--- Ders2/my-app/App.tsx
+++ Ders2/my-app/App.tsx
@@ -4,12 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-red-400">
-      <Text className='text-[#fff] text-3 xl'>Merhaba Metin </Text>
+      <Text className='text-[#fff] text-lg'>Merhaba Metin </Text>
       <StatusBar style="auto" />
-
       <View>
         <Text>Tamam</Text>
       </View>
+
     </View>
   );
 }
